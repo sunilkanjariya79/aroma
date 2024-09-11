2e8ebf139dbcba625d7950f1066b3639e62e31d7
sunilkanjariya79 <sunilkanjariya79@gmail.com>
2024-09-11 09:56:15 +0530

    syntax error solved 2

--- include/conn.php
+++ include/conn.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-$db=mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME) or die("database not connected");
 const DB_NAME='aroma';
 const DB_HOST='localhost';
 const DB_USER='root';
 const DB_PASS='';
+$db=mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME) or die("database not connected");
 ?>
