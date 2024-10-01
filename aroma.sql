a7a4cfab09376d3ab678eeb94db9ed832cff4839
ronit-911 <nnnronit@gmail.com>
2024-10-01 10:02:48 +0530

    database updated

--- aroma.sql
+++ aroma.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Sep 11, 2024 at 11:44 AM
+-- Generation Time: Oct 01, 2024 at 04:30 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -38,11 +38,11 @@ CREATE TABLE IF NOT EXISTS `block` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `book-post`
+-- Table structure for table `book_post`
 --
 
-DROP TABLE IF EXISTS `book-post`;
-CREATE TABLE IF NOT EXISTS `book-post` (
+DROP TABLE IF EXISTS `book_post`;
+CREATE TABLE IF NOT EXISTS `book_post` (
   `bid` int NOT NULL AUTO_INCREMENT,
   `uid` int NOT NULL,
   `bcover` varchar(255) NOT NULL,
@@ -52,16 +52,23 @@ CREATE TABLE IF NOT EXISTS `book-post` (
   `babout` varchar(180) NOT NULL,
   `bdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`bid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `book_post`
+--
+
+INSERT INTO `book_post` (`bid`, `uid`, `bcover`, `btitle`, `btag`, `bcontent`, `babout`, `bdate`) VALUES
+(1, 8, '1727330067loki.jpg', 'Some Random Story', 'book-story', '66f4f7139c9b4.html', 'this book is just with random story, nothing special about it, it says some random story taken from internet to try how this actually works', '2024-09-26 05:54:27');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `casual-post`
+-- Table structure for table `casual_post`
 --
 
-DROP TABLE IF EXISTS `casual-post`;
-CREATE TABLE IF NOT EXISTS `casual-post` (
+DROP TABLE IF EXISTS `casual_post`;
+CREATE TABLE IF NOT EXISTS `casual_post` (
   `pid` int NOT NULL AUTO_INCREMENT,
   `uid` int NOT NULL,
   `ptitle` varchar(50) NOT NULL,
@@ -69,7 +76,20 @@ CREATE TABLE IF NOT EXISTS `casual-post` (
   `pcontent` varchar(255) NOT NULL,
   `pdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`pid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `casual_post`
+--
+
+INSERT INTO `casual_post` (`pid`, `uid`, `ptitle`, `ptag`, `pcontent`, `pdate`) VALUES
+(1, 5, 'a', 'a', '66ec40698cee4.html', '2024-09-19 15:20:52'),
+(2, 5, 'hello', 'greeting', '66ec41fe51727.html', '2024-09-19 15:23:42'),
+(3, 5, 'hello 2', 'greeting 2', '66ec4481546b1.html', '2024-09-19 15:34:25'),
+(4, 5, 'hello 3', 'greeting 3', '66ec44f10066e.html', '2024-09-19 15:36:17'),
+(5, 5, 'hello, my first post', 'introducti', '66ed8e95de2d7.html', '2024-09-20 15:35:45'),
+(6, 8, 'hi', 'intro', '66f23ec4b5d34.html', '2024-09-24 04:23:32'),
+(7, 8, 'title', 'tag', '66f4e7409dde1.html', '2024-09-26 04:46:56');
 
 -- --------------------------------------------------------
 
@@ -99,7 +119,22 @@ CREATE TABLE IF NOT EXISTS `follower` (
   `uid` int NOT NULL,
   `follower` int NOT NULL,
   PRIMARY KEY (`fid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `follower`
+--
+
+INSERT INTO `follower` (`fid`, `uid`, `follower`) VALUES
+(1, 5, 8),
+(2, 8, 5),
+(3, 4, 8),
+(4, 1, 8),
+(5, 2, 8),
+(6, 3, 8),
+(7, 7, 8),
+(8, 8, 9),
+(9, 5, 9);
 
 -- --------------------------------------------------------
 
@@ -182,14 +217,30 @@ CREATE TABLE IF NOT EXISTS `users` (
   `umail` varchar(30) NOT NULL,
   `username` varchar(20) NOT NULL,
   `uname` varchar(255) NOT NULL,
-  `uprofile_photo` varchar(255) NOT NULL,
+  `gender` int NOT NULL,
+  `udate` date NOT NULL,
+  `uprofile_photo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'profile.jpeg',
   `uabout` varchar(320) NOT NULL,
   `upassword` varchar(20) NOT NULL,
   `udate_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `udate_updated` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
   `ulast_login` timestamp NOT NULL,
   PRIMARY KEY (`uid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`uid`, `umail`, `username`, `uname`, `gender`, `udate`, `uprofile_photo`, `uabout`, `upassword`, `udate_created`, `udate_updated`, `ulast_login`) VALUES
+(1, 'nnnronit@gmail.com', 'name', 'name', 1, '2005-09-13', 'profile.jpeg', 'about', 'pass', '2024-09-12 17:02:25', '2024-09-18 05:21:09', '0000-00-00 00:00:00'),
+(2, 'nnnronit@gmail.com', 'name', 'name', 1, '2005-09-13', 'profile.jpeg', 'about', 'pass', '2024-09-12 17:02:25', '2024-09-18 05:26:36', '0000-00-00 00:00:00'),
+(3, 'ronit@gmail.com', 'username', 'name', 1, '2002-05-12', 'profile.jpeg', 'this is about me', 'pass', '2024-09-17 13:17:03', '2024-09-18 05:26:40', '0000-00-00 00:00:00'),
+(4, 'ronit@gmail.com', 'username', 'name', 1, '2002-05-12', 'profile.jpeg', 'this is about me', 'pass', '2024-09-17 13:17:03', '2024-09-18 05:26:45', '0000-00-00 00:00:00'),
+(5, 'sss@a', 'sss', 'vv', 1, '1998-05-12', '1726674165loki.jpg', 'ssss', 'ss', '2024-09-18 03:43:29', '2024-09-18 15:42:45', '0000-00-00 00:00:00'),
+(8, 'mail@m', 'b', 'b', 1, '2005-05-05', 'profile.jpeg', 'b', 'b', '2024-09-24 04:23:11', '2024-09-26 04:39:36', '0000-00-00 00:00:00'),
+(7, 'a@gmail', 'a', 'a', 1, '2006-05-04', 'profile.jpeg', 'about', 'a', '2024-09-18 05:24:27', '2024-09-18 05:26:49', '0000-00-00 00:00:00'),
+(9, 'mymail@mail.com', 'something', 'name1', 1, '2005-09-13', '', 'i only want to say that i am me, and me is i', 'password', '2024-09-30 03:54:08', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
